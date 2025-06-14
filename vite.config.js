6e730739cf8624e7df3d32411b3fbd63ea4761f2
Daniel Taiakin <daniel@pewpee.com>
2025-06-15 02:52:23 +0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/nexari-mini-app/',
+  build: {
+    target: 'es2022'
+  },
+  esbuild: {
+    target: 'es2022'
+  }
 })
