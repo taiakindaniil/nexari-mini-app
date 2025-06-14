4a42c0a785913673b1dcadb06207ab6bd7c51ea0
Daniel Taiakin <daniel@pewpee.com>
2025-06-15 02:46:06 +0300

    feat: telegram sdk

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 import './index.css'
 import App from './App.jsx'
+import { init } from './Init.ts'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Mock the environment in case, we are outside Telegram.
+import './mockEnv.ts';
+
+const root = createRoot(document.getElementById('root'));
+
+try {
+  const launchParams = retrieveLaunchParams();
+  const { tgWebAppPlatform: platform } = launchParams;
+  const debug = (launchParams.tgWebAppStartParam || '').includes('platformer_debug')
+    || import.meta.env.DEV;
+
+  // Configure all application dependencies.
+  await init({
+    debug,
+    eruda: debug && ['ios', 'android'].includes(platform),
+    mockForMacOS: platform === 'macos',
+  }).then(() => {
+    root.render(
+      <StrictMode>
+        <App/>
+      </StrictMode>,
+    );
+  })
+} catch (e) {
+  console.error(e);
+  root.render(<div>Error</div>);
+}
